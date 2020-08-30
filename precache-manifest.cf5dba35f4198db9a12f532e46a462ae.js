62c2d9b80466629bad5a7db80d8d1eb3cd8c425c
Olga Kuznetsova <olya.kuznetsova.2015@list.ru>
2020-08-30 22:14:53 +0300

    Updates

---
+++ precache-manifest.cf5dba35f4198db9a12f532e46a462ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76bb3898c9f0134bc552959eb56ce71",
+    "url": "/project_task.github.io/index.html"
+  },
+  {
+    "revision": "74c56a9a07654b81f94e",
+    "url": "/project_task.github.io/static/css/main.19b6c9ce.chunk.css"
+  },
+  {
+    "revision": "66b706e6163768bc2a36",
+    "url": "/project_task.github.io/static/js/2.585c15c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project_task.github.io/static/js/2.585c15c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c56a9a07654b81f94e",
+    "url": "/project_task.github.io/static/js/main.b81279cb.chunk.js"
+  },
+  {
+    "revision": "822bfe111f60ccd258ab",
+    "url": "/project_task.github.io/static/js/runtime-main.8e111b32.js"
+  },
+  {
+    "revision": "d9eebe02a58908dd0faf7564baac955d",
+    "url": "/project_task.github.io/static/media/clock.d9eebe02.jpg"
+  }
+]);
